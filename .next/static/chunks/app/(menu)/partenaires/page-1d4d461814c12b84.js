2cb8cd3adad9ebff8fffebb8e87c15c8ba51387f
Zied Riahi <ziedriahi715@gmail.com>
2025-09-18 19:03:19 +0100

    Initiall commit

--- .next/static/chunks/app/(menu)/partenaires/page-1d4d461814c12b84.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[172,153,148],{8377:function(n,e,u){Promise.resolve().then(u.t.bind(u,5878,23))}},function(n){n.O(0,[878,971,997,744],function(){return n(n.s=8377)}),_N_E=n.O()}]);
