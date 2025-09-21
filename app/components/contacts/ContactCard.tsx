dac1b358c59be9eb0f05b2912d0df81c0f4afe67
Melek ElMokhtar <melekelmokhtar@gmail.com>
2025-09-21 19:39:25 +0100

    revision 1.4

--- app/components/contacts/ContactCard.tsx
+++ app/components/contacts/ContactCard.tsx
@@ -11,7 +11,7 @@ const ContactCard = ({
 }) => {
   return (
     <div className="bg-white m-6 border-2  border-primary rounded-lg p-6 max-w-2xl ">
-      <h2 className="text-green-600 text-xl font-semibold mb-4 text-center">
+      <h2 className="text-[#6D9161] text-xl font-semibold mb-4 text-center">
         {title}
       </h2>
       <ul className="space-y-2">
