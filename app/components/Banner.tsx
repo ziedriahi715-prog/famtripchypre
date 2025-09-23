764554f275f0865045f44e5c9e923f795ca8fe15
Melek ElMokhtar <melekelmokhtar@gmail.com>
2025-09-23 13:32:01 +0100

    bannnerrr

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -14,25 +14,14 @@ const Banner: React.FC<Banner> = ({
   return (
     <div
       className={`
-        relative bg-black bg-[url('/images/banniere.jpg')] bg-cover bg-center bg-no-repeat px-4 h-[33vh] md:h-[40vh] ${className} 
+        relative bg-[url('/images/banniere.png')] bg-cover bg-center bg-no-repeat px-4 h-[33vh] md:h-[40vh] ${className} 
        `}
     >
       {/* Dark overlay for better contrast */}
-      <div className="absolute inset-0 bg-black/30 z-0"></div>
 
       {/* THE MICE EXPERT section in top right */}
 
       {/* Main title at bottom center */}
-      <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 z-10 px-4 sm:bottom-4 md:bottom-6">
-        <div className="text-white text-center font-Scotch">
-          <div className="text-lg sm:text-2xl md:text-3xl lg:text-2xl xl:text-xl font-Bonnet font-bold uppercase tracking-[0.2em] leading-tight letter-shadow">
-            FAM TRIP CHYPRE
-          </div>
-          <div className="whitespace-nowrap text-lg sm:text-xl md:text-2xl lg:text-lg xl:text-base font-Scotch font-normal uppercase tracking-[0.15em] mt-2 leading-tight letter-shadow-subtitle">
-            L'ÉVASION GRANDEUR NATURE
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
